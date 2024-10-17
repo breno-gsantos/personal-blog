986a45c5ba3898791f6754b11a66531efedbe3d4
Breno Giovanni <brenogiovanniolintho@gmail.com>
2024-10-17 17:23:07 -0300

    Second commit

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -1,9 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
 import { ArrowLeft } from "lucide-react";
-import Link from "next/link";
 
 export default function PostDetail() {
   return (
